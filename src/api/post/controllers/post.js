27f32b2c40378bbbc61be322d52a8084cec93d54
kmishmael <kmishmael@gmail.com>
2024-06-07 10:55:45 +0300

    initial commit

---
+++ src/api/post/controllers/post.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * post controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::post.post' /*({ strapi }) => ({
+    // Query by slug
+    async findOne(ctx) {
+        // thanks to the custom route we have now a slug variable
+        // instead of the default id
+        const { slug } = ctx.params;
+        const entity = await strapi.db.query('api::post.post').findOne({
+            where: { slug }
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+    },
+})
+*/
+);
