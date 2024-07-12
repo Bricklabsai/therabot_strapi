04030c33bab0df401e51214a6d60d3e6cd6da9c8
kmishmael <kmishmael@gmail.com>
2024-07-12 19:14:42 +0300

    chore: enable product query by slug

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -4,4 +4,18 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController('api::product.product', ({ strapi }) => ({
+    // Query by slug
+    async findOne(ctx) {
+
+        const { id } = ctx.params;
+        let slug = id;
+        ctx.params['slug'] = slug;
+        const entity = await strapi.db.query('api::product.product').findOne({
+            where: { slug }, populate: true,
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+    },
+}));
